597857188caf47607bb37bf5262dc045f34b04e5
chenpian <1837890880@qq.com>
2016-10-12 16:15:36 +0800

    init

--- src/test/java/com/keeper/test/HttpClientTest.java
+++ src/test/java/com/keeper/test/HttpClientTest.java
@@ -1,8 +1,105 @@
 package com.keeper.test;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
 public class HttpClientTest {
-	
-	
-	
-	
+
+	@Test
+	public void test1() throws KeyManagementException, NoSuchAlgorithmException, IOException {
+		CookieStore cookieStore = new BasicCookieStore();
+		SSLContext sslcontext = createIgnoreVerifySSL();
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[] { "TLSv1" }, null,
+				SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+		RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
+		CloseableHttpClient httpclient = HttpClients.custom().setDefaultRequestConfig(globalConfig).setSSLSocketFactory(sslsf).build();
+		HttpClientContext localContext = HttpClientContext.create();
+		localContext.setCookieStore(cookieStore);
+		try {
+			HttpGet httpget = new HttpGet("https://xueqiu.com");
+			System.out.println("Executing request " + httpget.getRequestLine());
+			CloseableHttpResponse response = httpclient.execute(httpget, localContext);
+			try {
+				HttpEntity entity = response.getEntity();
+				System.out.println("----------------------------------------");
+				System.out.println(response.getStatusLine());
+				EntityUtils.consume(entity);
+			} finally {
+				response.close();
+			}
+			HttpUriRequest request = RequestBuilder.post("https://xueqiu.com/user/login")
+					.addParameter("password", "12AE41E7F5C3D68A50A79B44B2EFCCF4").addParameter("areacode", "86").addParameter("remember_me", "on")
+					.addParameter("telephone", "15827213122").build();
+			response = httpclient.execute(request, localContext);
+			try {
+				HttpEntity entity = response.getEntity();
+				System.out.println("----------------------------------------");
+				System.out.println(response.getStatusLine());
+				EntityUtils.consume(entity);
+			} finally {
+				response.close();
+			}
+			httpget = new HttpGet("https://xueqiu.com/cubes/rebalancing/history.json?cube_symbol=ZH117425&count=20&page=1");
+			System.out.println("Executing request " + httpget.getRequestLine());
+			response = httpclient.execute(httpget, localContext);
+			try {
+				HttpEntity entity = response.getEntity();
+				System.out.println("----------------------------------------");
+				System.out.println(response.getStatusLine());
+				System.out.println(EntityUtils.toString(entity));
+			} finally {
+				response.close();
+			}
+
+		} finally {
+			httpclient.close();
+		}
+	}
+
+	public SSLContext createIgnoreVerifySSL() throws NoSuchAlgorithmException, KeyManagementException {
+		SSLContext sc = SSLContext.getInstance("SSLv3");
+
+		X509TrustManager trustManager = new X509TrustManager() {
+			@Override
+			public void checkClientTrusted(java.security.cert.X509Certificate[] paramArrayOfX509Certificate, String paramString)
+					throws CertificateException {
+			}
+
+			@Override
+			public void checkServerTrusted(java.security.cert.X509Certificate[] paramArrayOfX509Certificate, String paramString)
+					throws CertificateException {
+			}
+
+			@Override
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+
+		sc.init(null, new TrustManager[] { trustManager }, null);
+		return sc;
+	}
+
 }
